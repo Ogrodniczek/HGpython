ca1f99ba19791b043f2a54db4afe2b6e82802fc3
Kodii <kordian.stryczek@gmail.com>
2014-11-13 18:31:47 +0100

    chyba wszystko juz OKi :D

--- python2-klasy/zadanie1.py
+++ python2-klasy/zadanie1.py
@@ -14,35 +14,31 @@ class Produkt(object):
 
     def sprzedaj_sztuke(self):
         self.aktualna_ilosc = self.aktualna_ilosc - 1
-            if self.aktualna_ilosc < 0:
-                print('ilosc w magazynie nie moze byc ujemna')
+        if self.aktualna_ilosc < 0:
+             print('ilosc w magazynie nie moze byc ujemna')
 
     def sprzedaj_sztuki(self, ilosc):
         self.aktualna_ilosc = self.aktualna_ilosc - ilosc
-            if self.aktualna_ilosc < 0:
-                print('ilosc w magazynie nie moze byc ujemna')
+        if self.aktualna_ilosc < 0:
+            print('ilosc w magazynie nie moze byc ujemna')
 
     def sprawdz_aktualna_ilosc(self):
         print('Aktualna ilosc to {}'.format(self.aktualna_ilosc))
 
     def sprawdz_srednia_cene(self):
-        srednia_cena = 0
-        ilosc_cen = 0
-        for i in range(len(self.historia_cen)):
-            srednia_cena = srednia_cena + self.historia_cen[i]
-            ilosc_cen = ilosc_cen + 1
-        print('Srednia cena wynosi: ', srednia_cena/ilosc_cen)
+        suma_cen = sum(self.historia_cen)
+        ilosc_cen = len(self.historia_cen)
+        print('Srednia cena wynosi: ', suma_cen/ilosc_cen)
 
     def pokaz_historie_cen(self):
-        for i in range(len(self.historia_cen)):
-            print(i, ': ', self.historia_cen[i])
+        print('Historia cen: ', self.historia_cen)
 
 #Przyklad uzycia
 #def przyklad():
 produkt1 = Produkt('zeszyt')
 produkt1.dodaj_ilosc(5)
-produkt1.zmien_cene(5)
-produkt1.zmien_cene(10)
+produkt1.zmien_cene(5.2)
+produkt1.zmien_cene(10.1)
 produkt1.sprzedaj_sztuke()
 produkt1.sprawdz_aktualna_ilosc()
 produkt1.pokaz_historie_cen()
