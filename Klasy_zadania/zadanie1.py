9684a04a219a45d74d72b08e911ef2a530be58c2
CokeBadger <zryty.kolec@gmail.com>
2014-11-13 14:54:45 +0100

    ...

--- Klasy_zadania/zadanie1.py
+++ Klasy_zadania/zadanie1.py
@@ -4,13 +4,18 @@ class Produkt(object):
         self.aktualna_cena = 0
         self.aktualna_ilosc = 0
     def dodaj_ilosc(self, ilosc):
-        self.aktualna_ilosc = self.aktualna_ilosc + ilosc
+        if ilosc == type(int):
+            if ilosc > 0:
+                self.aktualna_ilosc = self.aktualna_ilosc + ilosc
+            else:
+                print('Podaj liczbe calkowita wieksza od 0')
+        else: 
+            print('Podaj liczbe calkowita')
     def zmien_cene(self, nowa_cena):
         self.aktualna_cena = nowa_cena
     def sprzedaj_szuke(self):
-        if self.aktualna_ilosc > 1:
         self.aktualna_ilosc = self.aktualna_ilosc - 1
     def sprzedaj_sztuki(self, ilosc):
         self.aktualna_ilosc = self.aktualna_ilosc - ilosc
     def sprawdz_aktualna_ilosc(self):
-        print('Aktualna ilosc = {}',.format(self.aktualna_ilosc)
+        print('Aktualna ilosc = {}'.format(self.aktualna_ilosc))
