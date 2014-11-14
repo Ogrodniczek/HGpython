acc4c43ebd43c2a12bc7565a2a0ebfe3632642cb
CokeBadger <zryty.kolec@gmail.com>
2014-11-15 00:26:26 +0100

    zadanie1 dziala

--- PS_SD_zadania/zadanie1.py
+++ PS_SD_zadania/zadanie1.py
@@ -1,12 +1,13 @@
 """Program sprawdza czy wprowadzna liczba calkowia jest wieksza lub rowna 10"""
 
-while True:
-    try:
-        spr = int(input('Podaj liczbe calkowita : '))
-        if  spr >= 10:
-            print('Jestes zwyciezca')
-        else:
-            print('umrzyj')
-        break
-    except ValueError:
-        print('To nie jest liczna calkowita')
+if __name__ == '__main__':
+    while True:
+        try:
+            sprawdz_liczbe = int(input('Podaj liczbe calkowita : '))
+            if  sprawdz_liczbe >= 10:
+                print('Jestes zwyciezca')
+            else:
+                print('umrzyj')
+            break
+        except ValueError:
+            print('To nie jest liczna calkowita')
