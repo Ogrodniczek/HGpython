ae40b8d3b5ada306e577c145c8400c0c736073f5
CokeBadger <zryty.kolec@gmail.com>
2014-11-15 00:04:18 +0100

    dziala

--- PS_SD_zadania/zadanie1.py
+++ PS_SD_zadania/zadanie1.py
@@ -1,6 +1,10 @@
-print('Podaj liczbe : ')
-x = int(input())
-if x >= 10.0:
-    print('Jestes zwyciezca')
-else:
-    print('Umrzyj')
+while True:
+    try:
+        x = int(input('Podaj liczbe calkowita : '))
+        if x >= 10:
+            print('Jestes zwyciezca')
+        else:
+            print('umrzyj')
+        break
+    except ValueError:
+        print('To nie jest liczna calkowita')
