cbfb988408eaf082f01c98ef6bdf98f27a4482a8
CokeBadger <zryty.kolec@gmail.com>
2014-11-15 00:54:27 +0100

    fuck yeah

---
+++ PS_SD_zadania/zadanie2.py
@@ -0,0 +1,13 @@
+"""Program sumuje liczby podane przez uzytkownika do momentu az suma bedzie wieksza od 100"""
+
+if __name__ == '__main__':
+    suma = float(0)
+    while suma <= 100:
+        while True:
+            try:
+                liczba =  float(input('Podaj liczbe : '))
+                break
+            except ValueError:
+                print('Wprowadzona wartosc nie jest liczba')
+        suma = suma + liczba
+    print(suma)
