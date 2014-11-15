f0b386957e4cf244b2e51c4685d3e8a1defec8f2
CokeBadger <zryty.kolec@gmail.com>
2014-11-15 01:02:41 +0100

    zadanie3

---
+++ PS_SD_zadania/zadanie3.py
@@ -0,0 +1,7 @@
+"""Program wypisuje wszystkie liczby parzyste od 0 do 100 włącznie"""
+
+if __name__ == '__main__':
+    parzysta = int(-2)
+    while parzysta < 100:
+        parzysta = parzysta +2
+        print(parzysta)
