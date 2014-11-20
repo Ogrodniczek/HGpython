c3ae1e7b8823aa0ac0283356d487b2ffa35a19c3
CokeBadger <zryty.kolec@gmail.com>
2014-11-20 23:26:08 +0100

    zadanie4

--- PS_SD_zadania/zadanie4.py
+++ PS_SD_zadania/zadanie4.py
@@ -2,12 +2,25 @@
 
 if __name__ == '__main__':
     ilosc_liczb = int(0)
+    tablica = []
+    odwrotna_tablica = []
     while ilosc_liczb <= 0:
         while True:
             try:
                 ilosc_liczb = int(input('Podaj ilosc liczb : '))
                 break
             except ValueError:
-                print('Wprowadzona wartosc nie jest liczba calkowita')
-        except False:
-            print('lawl')
+                pass
+    for i in range(0, ilosc_liczb):
+        while True:
+            try:
+                liczba =  int(input('Podaj liczbe nr ' + str(i+1) + ' : ' ))
+                break
+            except ValueError:
+                pass
+        tablica.append(liczba)
+    for n in range(0, ilosc_liczb):
+        odwrotna_liczba = - int(tablica[n])
+        odwrotna_tablica.append(odwrotna_liczba)
+    for x in range(0, ilosc_liczb):
+        print(tablica[x], odwrotna_tablica[x])
