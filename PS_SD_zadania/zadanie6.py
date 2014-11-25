e6601e3169f874486350b6aaa527a13d4ba850cd
CokeBadger <zryty.kolec@gmail.com>
2014-11-25 21:21:59 +0100

    zad6 + pep8

---
+++ PS_SD_zadania/zadanie6.py
@@ -0,0 +1,22 @@
+"""Program zwraca liczby fibonacciego z przedzialu od 10 do 20"""
+
+
+def ciag_fibonacciego():
+
+    """petla"""
+
+    fib1 = 0
+    fib2 = 1
+    fib = 0
+    tfib = []
+    while fib <= 20:
+        fib = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib
+        if fib >= 10 and fib <= 20:
+            tfib.append(fib)
+    print(tfib)
+
+
+if __name__ == '__main__':
+    ciag_fibonacciego()
