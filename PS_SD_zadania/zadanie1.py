d273311f6f6c623d1b367c584304d2d5ba87bcfe
CokeBadger <zryty.kolec@gmail.com>
2014-11-26 05:00:05 +0100

    Pycharm KURWO!

--- PS_SD_zadania/zadanie1.py
+++ PS_SD_zadania/zadanie1.py
@@ -1,13 +1,14 @@
+# encoding: utf-8
 """Program sprawdza czy wprowadzna liczba calkowia jest wieksza lub rowna 10"""
 
-if __name__ == '__main__':
-    while True:
-        try:
-            sprawdz_liczbe = int(input('Podaj liczbe calkowita : '))
-            if  sprawdz_liczbe >= 10:
-                print('Jestes zwyciezca')
-            else:
-                print('umrzyj')
-            break
-        except ValueError:
-            print('To nie jest liczna calkowita')
+
+while True:
+    try:
+        sprawdz_liczbe = int(input('Podaj liczbe calkowita : '))
+        if sprawdz_liczbe >= 10:
+            print('Jestes zwyciezca')
+        else:
+            print('umrzyj')
+        break
+    except ValueError:
+        print('To nie jest liczna calkowita')
