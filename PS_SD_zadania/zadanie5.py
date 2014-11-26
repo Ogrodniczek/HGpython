9da9ed4de52e90ad5ee401c81405e40701c79c01
CokeBadger <zryty.kolec@gmail.com>
2014-11-26 04:52:19 +0100

    cos nowego

--- PS_SD_zadania/zadanie5.py
+++ PS_SD_zadania/zadanie5.py
@@ -1,18 +1,18 @@
 # encoding: utf-8
-"""Program wypisuje uzytkownikow z systemu i ich domyslna powloke"""
+"""List of users and their default directory"""
 
 
-def lista_uzytkownikow():
-    """funkcja zwaraca uzytkowniko i powloke systemu w formie listy"""
-    tablica = []
-    handler = open('/etc/passwd', 'r')
-    for users in handler:
-        tablica.append(users.split(":"))
+def user_list():
+    """getting list of users from file"""
+    users = []
+    handler = open('/etc/passwd')
+    for lines_in_passwd in handler:
+        users.append(lines_in_passwd.split(":"))
     n = 0
-    while n+1 <= len(tablica):
-        print(tablica[n][0], " : ", tablica[n][5])
+    while n+1 <= len(users):
+        print(users[n][0], " : ", users[n][5])
         n += 1
 
 
 if __name__ == '__main__':
-    lista_uzytkownikow()
+    user_list()
