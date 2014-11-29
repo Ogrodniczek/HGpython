4e2069c9daa1a3da01ca159b6e2b48c43801f70d
CokeBadger <zryty.kolec@gmail.com>
2014-11-29 05:58:49 +0100

    refactor

--- Klasy_zadania/zadanie1.py
+++ Klasy_zadania/zadanie1.py
@@ -1,30 +1,42 @@
-class Produkt(object):
-    def __init__(self, nazwa):
-        self.nazwa = nazwa
-        self.aktualna_cena = 0
-        self.aktualna_ilosc = 0
-    def dodaj_ilosc(self, ilosc):
-        if type(ilosc) == int:
-            if ilosc > 0:
-                self.aktualna_ilosc = self.aktualna_ilosc + ilosc
+# encoding: utf-8
+"""class for storing products"""
+
+
+class Product(object):
+
+    """main class"""
+
+    def __init__(self, name):
+        self.name = name
+        self.actual_price = 0
+        self.actual_quantity = 0
+
+    def add_quantity(self, quantity):
+        if type(quantity) == int:
+            if quantity > 0:
+                self.actual_quantity = self.actual_quantity + quantity
             else:
                 print('Podaj liczbe calkowita wieksza od 0')
         else: 
             print('Podaj liczbe calkowita')
-    def zmien_cene(self, nowa_cena):
-        if type(nowa_cena) == float:
-            self.aktualna_cena = nowa_cena
+
+    def change_price(self, new_price):
+        if type(new_price) == float:
+            self.actual_price = new_price
         else:
             print('zly format ceny')
-    def sprzedaj_szuke(self):
-        if self.aktualna_ilosc > 0:
-            self.aktualna_ilosc = self.aktualna_ilosc - 1
+
+    def sell_one(self):
+        if self.actual_quantity > 0:
+            self.actual_quantity -= 1
         else:
             print('brak w magazynie')
-    def sprzedaj_sztuki(self, ilosc):
-        if self.aktualna_ilosc >= ilosc:
-            self.aktualna_ilosc = self.aktualna_ilosc - ilosc
+
+    def sell_x_pieces(self, quantity):
+        if self.actual_quantity >= quantity:
+            self.actual_quantity = self.actual_quantity - quantity
         else:
             print('brak odpowiedniej ilosci w magazynie')
-    def sprawdz_aktualna_ilosc(self):
-        print('Aktualna ilosc = {}'.format(str(self.aktualna_ilosc)))
+
+    def check_actual_price(self):
+        print('Aktualna ilosc = {}'.format(str(self.actual_quantity)))
