39495e09db9a1e0df7797b96e4a260b8f23d258d
krzaky <fd1192@hotmail.com>
2014-11-29 10:34:48 -0500

    zadanie2.py

---
+++ zadania/zadanie2.py
@@ -0,0 +1,15 @@
+suma_liczb = 0
+while True:
+    dana_liczba = int(input('Podaj liczbe'))
+
+    suma_liczb += dana_liczba
+
+    if suma_liczb <= 100:
+        print('Wiecej')
+    else:
+        print('koniec')
+        break
+
+            
+
+
