f065007d76e727da9ac095b348c4e22098585521
krzaky <fd1192@hotmail.com>
2014-11-29 08:56:26 -0500

    pierwsze_zadanie

---
+++ zadania/zadanie.1.py
@@ -0,0 +1,18 @@
+
+dana_liczba = int(input('Podaj liczbe: '))
+
+if dana_liczba <= 1:
+    dana_liczba = 0
+    print(' < 10')
+
+elif dana_liczba <= 5:
+    print('Chcialbys miec wiecej')    
+elif dana_liczba <= 7:
+    print ('juz wiecej ale dalej nie 10')
+
+else:
+    dana_liczba > 10
+    print(' > 10 ')
+
+
+   
