ae30aa24d94f2293c9a1e548129308952e06e9cd
CokeBadger <zryty.kolec@gmail.com>
2014-11-30 08:30:21 +0100

    GOMLB

--- PS_SD_zadania/zadanie2.py
+++ PS_SD_zadania/zadanie2.py
@@ -1,6 +1,6 @@
 """Program sumuje liczby podane przez uzytkownika do momentu az suma bedzie wieksza od 100"""
 
-if __name__ == '__main__':
+def suma_liczb():
     suma = float(0)
     while suma <= 100:
         while True:
@@ -10,3 +10,6 @@ if __name__ == '__main__':
             except ValueError:
                 print('Wprowadzona wartosc nie jest liczba')
     print(suma)
+
+if __name__ == "__main__":
+    suma_liczb()
