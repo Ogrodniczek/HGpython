953b7fdf3ee493c7e2c387003f34f4e6d63d93f5
CokeBadger <zryty.kolec@gmail.com>
2014-12-01 00:19:04 +0100

    naprawde chce mi sie spac

--- Klasy_zadania/zadanie1.py
+++ Klasy_zadania/zadanie1.py
@@ -8,6 +8,8 @@ class Product(object):
         self.name = name
         self.actual_price = 0
         self.actual_quantity = 0
+        self.price_history = []
+        self.how_many_at_price = []
 
     def add_quantity(self, quantity):
         if type(quantity) == int:
@@ -23,18 +25,30 @@ class Product(object):
             self.actual_price = new_price
         else:
             print('zly format ceny')
+        self.price_history.append(new_price)
 
     def sell_one(self):
         if self.actual_quantity > 0:
             self.actual_quantity -= 1
         else:
             print('brak w magazynie')
+        self.how_many_at_price.append([self.actual_price, 1])
 
     def sell_x_pieces(self, quantity):
         if self.actual_quantity >= quantity:
             self.actual_quantity -= quantity
         else:
             print('brak odpowiedniej ilosci w magazynie')
+        self.how_many_at_price.append([self.actual_price, quantity])
 
     def check_actual_price(self):
         print('Aktualna ilosc = {}'.format(str(self.actual_quantity)))
+
+    def show_price_history(self):
+        print('Historia cen : ', self.price_history)
+
+    def show_average_price(self):
+        print('Srednia cena = ', sum(self.price_history) / len(self.price_history))
+
+    def sales_history(self):
+        print(self.how_many_at_price)
